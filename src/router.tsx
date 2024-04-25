4de235059991396f88da520699d89ef8bc9d53bb
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-25 18:15:51 -0400

    create-client

---
+++ src/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Product from "./views/Product";
+import NewProduct from "./views/NewProduct";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element : <Layout/>,
+        children: [
+            {
+                index: true,
+                element: <Product/>
+            },
+            {
+                path: 'products/new',
+                element: <NewProduct/>
+            }
+        ]
+    }
+])
