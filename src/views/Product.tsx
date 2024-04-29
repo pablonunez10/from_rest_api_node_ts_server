8f2a23b6289c5d2ff80413d4e937f56f275e3a17
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-04-29 18:25:15 -0400

    format & trycatch on loader

--- src/views/Product.tsx
+++ src/views/Product.tsx
@@ -3,49 +3,51 @@ import { getProducts, updateProductAvailability } from "../services/ProductServi
 import ProductDetail from "../Components/ProductDetail"
 import type { Product } from "../types"
 export async function loader() {
-  const products = await getProducts()
-
-  return products
+  try {
+    return await getProducts()
+  } catch (error) {
+    return []
+  }
 }
-export async function action({request} : ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData())
   await updateProductAvailability(+data.id)
-  return{}
+  return {}
 }
 
 export default function Product() {
-  const products = useLoaderData() as Product []
+  const products = useLoaderData() as Product[]
   return (
     <>
-        <div className="flex justify-between">
-            <h2 className="text-4xl font-black text-slate-500">Productos</h2>
-            <Link
-                to="products/new"
-                className="rounded-md bg-indigo-600 p-3 text-sm font-bld text-white shadow-sm hover:bg-indigo-500"
-            >
-                Agregar Producto
-            </Link>
-        </div>
-        <div className="p-2">
-          <table className="w-full mt-5 table-auto">
-            <thead className="bg-slate-800 text-white">
-              <tr>
-                <th className="p-2">Producto</th>
-                <th className="p-2">Precio</th>
-                <th className="p-2">Disponibilidad</th>
-                <th className="p-2">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map(product => (
-                <ProductDetail
-                  key={product.id}
-                  product = {product}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+        <Link
+          to="products/new"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bld text-white shadow-sm hover:bg-indigo-500"
+        >
+          Agregar Producto
+        </Link>
+      </div>
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <ProductDetail
+                key={product.id}
+                product={product}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
